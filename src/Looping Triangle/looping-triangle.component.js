c95ae93fc2de1b5fdd0246f3cfbc1dac23238c27
Finley <finley.blakeman@growersedge.com>
2023-09-19 16:36:01 -0500

    Looping a Triangle exercise with tests

--- src/Looping Triangle/looping-triangle.component.js
+++ src/Looping Triangle/looping-triangle.component.js
@@ -1,4 +1,14 @@
-function loopingTriangle(a) {
-    return a;
+function loopingTriangle(lineCount = 7, character = '#') {
+    let i = 1;
+    let line = '';
+    lineCount = lineCount === undefined ? 7 : lineCount;
+    character = character === undefined ? '#' : character;
+    
+    while (i < lineCount + 1) {
+        line += character.repeat(i) + '\n';
+        i++;
+    }
+    
+    return line;
 }
 module.exports = loopingTriangle;
--- src/Looping Triangle/looping-triangle.component.test.js
+++ src/Looping Triangle/looping-triangle.component.test.js
@@ -1,8 +1,16 @@
 const loopingTriangle = require('./looping-triangle.component');
 const {describe, it, expect, test} = require("@jest/globals");
 
-describe('', () => {
-    it('should', () =>{
-        expect(loopingTriangle(2)).toBe(2);
+describe('looping triangle function', () => {
+    it('should print the triangle with pound sign and 8 lines given no inputs', () => {
+        expect(loopingTriangle(undefined, undefined)).toBe("#\n" + "##\n" + "###\n" + "####\n" + "#####\n" + "######\n" + "#######\n");
+    });
+
+    it('should print the triangle with pound sign and 3 lines given only line count input of 3', () => {
+        expect(loopingTriangle(3, undefined)).toBe("#\n" + "##\n" + "###\n" );
+    });
+    
+    it('should print the triangle with dollar sign and 8 lines given character input of $ and line count of 7', () => {
+        expect(loopingTriangle(undefined, '$')).toBe("$\n" + "$$\n" + "$$$\n" + "$$$$\n" + "$$$$$\n" + "$$$$$$\n" + "$$$$$$$\n");
     });
 });
